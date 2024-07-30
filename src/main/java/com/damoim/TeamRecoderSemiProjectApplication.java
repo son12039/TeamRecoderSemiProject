99b62985133cfa0aca7349c3882420a7e20df7a6
Parkseongchul97 <dol9991@naver.com>
2024-07-30 17:18:38 +0900

    Initial commit

---
+++ src/main/java/com/damoim/TeamRecoderSemiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.damoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamRecoderSemiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamRecoderSemiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damoim/TeamRecoderSemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamRecoderSemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

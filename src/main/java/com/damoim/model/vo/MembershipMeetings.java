cd911283cf064c3cb394b1ca9202b29a8f1875e2
Parkseongchul97 <dol9991@naver.com>
2024-07-31 17:39:58 +0900

    추가전 저장
    0731

--- src/main/java/com/damoim/model/vo/MembershipMeetings.java
+++ src/main/java/com/damoim/model/vo/MembershipMeetings.java
@@ -15,5 +15,6 @@ public class MembershipMeetings {
     private int meetAgreeCode; // 참여여부 테이블 연결
     private String meetInfo; // 모임관련 정보
     private Date meetCreatDate; // 모임 생성일
+    private int listCode; // 모임 작성글 작성자
 }
 

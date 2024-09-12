c43dae98a83a82ae37aa63ec818ee553c1ea1efb
Parkseongchul97 <dol9991@naver.com>
2024-09-12 16:18:27 +0900

    Update RemoveMemberService.java

--- src/main/java/com/damoim/service/RemoveMemberService.java
+++ src/main/java/com/damoim/service/RemoveMemberService.java
@@ -18,29 +18,27 @@ public class RemoveMemberService {
 	@Autowired
 	private RemoveMemberMapper mapper;
 	
-	public void deleteMembershipUserList(String id) {
+	public void deleteMembershipUserList(String id) { // 해당 유저가 가입한 모든 클럽에서 탈퇴 처리
 		mapper.deleteMembershipUserList(id);
 	}
 	
-	public void deleteAllComment(String id) {
+	public void deleteAllComment(String id) {// 해당 ID로 작성된 모든 댓글 삭제 로직
 	
 		ArrayList<MainComment> mainList = mapper.selectMain(id);	
 		for(MainComment main : mainList) {
 			int count = mapper.mainReCommentCount(main.getMainCommentCode());
-				if(count == 0) { // 대댓글이 없으면
-					
+				if(count == 0) { // 해당 댓글의 대댓글이 없는 경우				
 					MainComment commentParents = mapper.selectMainComment(main.getMainParentsCommentCode());
 					if(mapper.mainReCommentCount(main.getMainParentsCommentCode()) == 1 && commentParents.getMainCommentText() == null) {
-						mapper.deleteMainComment(main.getMainCommentCode());
-						mapper.deleteMainComment(main.getMainParentsCommentCode());
+						mapper.deleteMainComment(main.getMainCommentCode());// 본인 삭제후
+						mapper.deleteMainComment(main.getMainParentsCommentCode());// 부모도 삭제
 					}else {
-						mapper.deleteMainComment(main.getMainCommentCode());
+						mapper.deleteMainComment(main.getMainCommentCode());// 본인 삭제
 					}
 				}else {
-					mapper.deleteUpdateMainComment(main.getMainCommentCode());
+					mapper.deleteUpdateMainComment(main.getMainCommentCode());// 삭제된 댓글입니다.!
 				}
-		}
-		
+		}	
 		ArrayList<MeetingsComment> meetList = mapper.selectMeet(id);
 		for(MeetingsComment meet : meetList) {
 			int count = mapper.meetingReCommentCount(meet.getMeetCommentCode());
@@ -78,6 +76,9 @@ public class RemoveMemberService {
 		}
 
 	}
+	
+	
+	
 	// 해당 미팅 글 삭제 눌렀을때 진행 로직
 	public void deleteMeeting(int MeetCode) {
 		mapper.deleteAllMeetComment(MeetCode); // 댓글 삭제
